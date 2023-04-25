95f58bf5da76ce090c3b23a36879584ef9705b15
HankGuo <52509416+HankGuo93@users.noreply.github.com>
2023-04-25 09:47:58 +0800

    feat : commentDto added a field [Id] and a function [CreateCommentPageResponse()]]

--- dtos/commentDto.go
+++ dtos/commentDto.go
@@ -1,6 +1,9 @@
 package dtos
 
+import "GoCommunityAPI/models"
+
 type CommentDto struct {
+	Id        int
 	Content   string
 	UserId    int
 	ArticleId int
@@ -9,4 +12,23 @@ type CommentDto struct {
 	DeletedAt int64
 
 	User UserDto
+}
+
+func CreateCommentPageResponse(comments []models.CommentModel) []map[string]interface{} {
+	var dtos = make([]map[string]interface{}, len(comments))
+	for index, model := range comments {
+		dtos[index] = map[string]interface{}{
+			"Id":        model.Id,
+			"ArticleId": model.ArticleId,
+			"Content":   model.Content,
+			"CreatedAt": model.CreatedAt,
+			"UpdatedAt": model.UpdatedAt,
+			"User": map[string]interface{}{
+				"Id":    model.User.Id,
+				"Name":  model.User.Name,
+				"Email": model.User.Email,
+			},
+		}
+	}
+	return dtos
 }
